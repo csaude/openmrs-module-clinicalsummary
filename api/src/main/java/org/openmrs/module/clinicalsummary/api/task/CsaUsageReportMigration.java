98ff38f2f4cc0a8de8804f4d4adaa206f6e269ab
Jose Julai Ritsure <ritsure.julai.jose@gmail.com>
2023-07-18 17:29:48 +0200

    Json post to DHIS2

--- api/src/main/java/org/openmrs/module/clinicalsummary/api/task/CsaUsageReportMigration.java
+++ api/src/main/java/org/openmrs/module/clinicalsummary/api/task/CsaUsageReportMigration.java
@@ -1,14 +1,21 @@
 package org.openmrs.module.clinicalsummary.api.task;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.clinicalsummary.api.ClinicalSummaryModuleService;
 import org.openmrs.module.clinicalsummary.api.model.CsaUsageReport;
 import org.openmrs.module.clinicalsummary.api.util.DateUtils;
-
-import java.io.IOException;
-import java.util.List;
 
 public class CsaUsageReportMigration {
 
@@ -21,38 +28,48 @@ public class CsaUsageReportMigration {
         this.csaUsageReportService = Context.getService(ClinicalSummaryModuleService.class);
     }
 
-    private boolean sendReport (CsaUsageReport csaUsageReport) throws IOException {
-       /* String URL = URLBase + URLPath;
+    private boolean sendReport (CsaUsageReport csaUsageReport) throws IOException, AuthenticationException {
+       String URLBase = "http://localhost:8099/dhis"; // 172.20.0.2//172.19.0.4:8080
+       String URLPath = "/api/events";
+       String URL = URLBase + URLPath;
         Boolean response = false;
         DefaultHttpClient httpclient = new DefaultHttpClient();
+        
         try {
             HttpPost httpPost = new HttpPost(URL);
             System.out.println(URL);
-            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
+            String username = "admin";
+            String password = "district";
+            //UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
             BasicScheme scheme = new BasicScheme();
-            Header authorizationHeader = scheme.authenticate(credentials, httpPost);
+            Header authorizationHeader = scheme.authenticate(new org.apache.http.auth.UsernamePasswordCredentials(username, password), httpPost);
             httpPost.setHeader(authorizationHeader);
-            httpPost.setEntity(input);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-Type", "application/json");
+            
+            httpPost.setEntity(new ByteArrayEntity(csaUsageReport.getJson().getBytes("UTF8")));
+            
+            //httpPost.setEntity((HttpEntity) csaUsageReport);
             // System.out.println("Executing request: " + httpGet.getRequestLine());
             // System.out.println(response);
             // response = httpclient.execute(httpGet,responseHandler);
-            HttpResponse responseRequest = httpclient.execute(httpPost);
+            HttpResponse responseRequest = (HttpResponse) httpclient.execute(httpPost);
 
-            if (responseRequest.getStatusLine().getStatusCode() != 204
-                    && responseRequest.getStatusLine().getStatusCode() != 201) {
+            if (((org.apache.http.HttpResponse) responseRequest).getStatusLine().getStatusCode() != 204
+                    && ((org.apache.http.HttpResponse) responseRequest).getStatusLine().getStatusCode() != 201) {
                 throw new RuntimeException(
-                        "Failed : HTTP error code : " + responseRequest.getStatusLine().getStatusCode());
+                        "Failed : HTTP error code : " + ((org.apache.http.HttpResponse) responseRequest).getStatusLine().getStatusCode());
             }
 
             httpclient.getConnectionManager().shutdown();
             response = true;
         } finally {
             httpclient.getConnectionManager().shutdown();
-        }*/
+        }
         return true;
     }
 
-    public void doMigration() throws IOException {
+    public void doMigration() throws IOException, AuthenticationException {
         log.info("Searching reports to send ...");
         this.usageReportList = this.doSearch();
 
@@ -77,7 +94,7 @@ public class CsaUsageReportMigration {
         return csaUsageReportService.getAllForMigration();
     }
 
-    private void updateMigrationStatus(CsaUsageReport csaUsageReport) {
+	private void updateMigrationStatus(CsaUsageReport csaUsageReport) {
         csaUsageReport.setMigrationStatus(CsaUsageReport.MIGRATED);
         csaUsageReport.setDateMigrated(DateUtils.getCurrentDate());
         csaUsageReportService.save(csaUsageReport);
